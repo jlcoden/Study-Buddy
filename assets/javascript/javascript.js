d03cf56b57cdc233e40139ffb1970f7663289523
Andrew Miller <206alm@gmail.com>
2019-10-12 13:52:19 -0700

    reworked calendar, whole thing is inside one flexbow now

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,15 +2,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
     dateFind();
   });
 
-
+var dayNames = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+];
 
 function dateFind() {
     var currentDate = new Date();
-    document.getElementById("dateHolder").innerHTML = currentDate.getDate();
-    console.log(currentDate);
-    console.log(typeof(currentDate));
-    var dateDiv = document.createElement("div");
-    var dateData = document.createTextNode(currentDate);
-    dateDiv.append(dateData);
+    var dayNumber = currentDate.getDate();
+    console.log('daynumber: ' + dayNumber);
+    var dayOfWeek = dayNames[currentDate.getDay()];
+    console.log('dayofweek: ' + dayOfWeek);
+    $('.col.' + dayOfWeek + ' .dayNumber').text(dayNumber);
+    // console.log(currentDate);
+    // console.log(typeof(currentDate));
+    // var dateDiv = document.createElement("div");
+    // var dateData = document.createTextNode(currentDate);
+    // dateDiv.append(dateData);
+
     //$('#dateHolder').append(dateDiv);
 }   
