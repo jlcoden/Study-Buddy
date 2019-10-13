65e59754d4c3ff7dede25d7441bbfdae5d8f68ce
Andrew Miller <206alm@gmail.com>
2019-10-13 16:56:48 -0700

    started the javascript. awaiting the meeting to figure out calnedar framework

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,31 @@
-  
+var daysOfWeek = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+];
+
+
+
+
+
+
+$(document).ready(function() {
+    console.log("im ready");
+    datePull();
+})  
+
+function datePull() {
+    let newDate = new Date();
+    let dateToday = newDate.getDate();
+    console.log(dateToday);
+    let dayToday = newDate.getDay();
+    console.log(dayToday);
+    let dayNum = parseInt(dayToday);
+    console.log(dayNum);
+    let dayName = daysOfWeek.indexOf(dayToday, 0);
+    console.log(dayName);
+}
