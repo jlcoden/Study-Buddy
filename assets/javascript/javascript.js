ce05a188d20706146c751f5d2a4c3ef491a87af8
Andrew Miller <206alm@gmail.com>
2019-10-16 17:11:06 -0700

    drag and drop working. next up - add card

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,54 @@
+var daysOfWeek = [
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday'
+];
+
+
+
+
+
+
+$(document).ready(function() {
+    console.log("im ready");
+    datePull();
+})  
+
+function datePull() {
+    let newDate = new Date();
+    let dateToday = newDate.getDate();
+    console.log(dateToday);
+    let dayToday = newDate.getDay();
+    console.log(dayToday);
+    let dayName = daysOfWeek[dayToday];
+    console.log(dayName);
+}
+
+function allowDropP(ev) {
+    ev.preventDefault();
+}
+function dragP(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+}
+function dropP(ev) {
+    ev.preventDefault();
+    console.log(ev);    
+    var data = ev.dataTransfer.getData('text');
+    console.log(data);
+    var copyButton = document.createElement('div');
+    copyButton.setAttribute('class', (data === 'preferredTemplate') ? 'buttonPreferred  ' : 'buttonCannot');
+    var original = document.getElementById(data);
+    copyButton.src = original.src;
+    ev.target.appendChild(copyButton);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
 console.log('Hello.');
+
+
+function allowDropP(ev) {
+    ev.preventDefault();
+}
+function dragP(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+}
+function dropP(ev) {
+    ev.preventDefault();
+    console.log(ev);    
+    var data = ev.dataTransfer.getData('text');
+    console.log(data);
+    // var copyButton = document.createElement('div');
+    let copyButton = $('<div>');
+    copyButton.addClass((data === 'preferredTemplate') ? 'preferred event-template' : 'busy event-template');
+    // copyButton.setAttribute('class', (data === 'preferredTemplate') ? '.busy.event-template' : 'buttonCannot');
+    // var original = document.getElementById(data);
+    // copyButton.src = original.src;
+    //ev.target.appendChild(copyButton);
+    //console.log(ev.target);
+    $(ev.target).append(copyButton);    
+}
