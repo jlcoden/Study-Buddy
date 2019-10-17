aa65fc35dc8d15e4491700e5d1a804493e760f66
Andrew Miller <206alm@gmail.com>
2019-10-16 17:50:19 -0700

    drag and drop to and all fields is functioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,23 @@ function dropP(ev) {
     // var copyButton = document.createElement('div');
     let copyButton = $('<div>');
     copyButton.addClass((data === 'preferredTemplate') ? 'preferred event-template' : 'busy event-template');
+    copyButton.addClass((data === 'preferredTemplate') ? 'preferredCard' : 'busyCard');
     // copyButton.setAttribute('class', (data === 'preferredTemplate') ? '.busy.event-template' : 'buttonCannot');
     // var original = document.getElementById(data);
     // copyButton.src = original.src;
     //ev.target.appendChild(copyButton);
     //console.log(ev.target);
     $(ev.target).append(copyButton);    
+}
+
+
+$(document).on('click', '.preferredCard', function() {
+    console.log("ive been clicked");
+    console.log(this);
+    $(this).attr('onclick', 'showCard()');
+})
+
+
+function showCard() {
+    console.log(this);
 }
